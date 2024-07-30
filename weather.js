9cf67b1bfcbd59add369988474579853467603b6
Myles <mylesvirgilcruz@gmail.com>
2024-07-30 15:31:36 -0700

    Display forecast for remaining hours in the day

--- weather.js
+++ weather.js
@@ -1,21 +1,52 @@
 const URL = "https://api.weatherapi.com/v1";
-const apiKey = "key=c92183db87384d8d806184544242907";
-const current = "/current.json";
-const forecast = "/forecast.json";
-const queryParam = "q=";
-const daysParam = "days="
+const KEY_FILE = './assets/key.txt';
+const API_KEY = "key=c92183db87384d8d806184544242907";
+const CURRENT = "/current.json";
+const FORECAST = "/forecast.json";
+const QUERY_PARAM = "q=";
+const DAYS_PARAM = "days=";
 
 function setCurrentTemp(response) {
     let tempF = document.getElementById('fahrenheit');
     tempF.innerHTML = Math.round(parseFloat(response.temp_f))  + "º";
+
+    let currentDate = Date.now();
+    let currentTime = new Date(currentDate);
+    let hours = currentTime.getHours();
+    if (hours > 19) {
+        let container = document.querySelector('.container');
+        container.style.cssText = "background-color: rgb(22, 22, 53)";
+    }
 }
 
-function setForecastTemp(response) {
+function setHighLow(response) {
     let today = document.querySelector('.today');
     let max = Math.round(parseFloat(response.maxtemp_f));
     let min = Math.round(parseFloat(response.mintemp_f));
 
     today.innerHTML = "H: " + max + "º | L: " + min + "º";
+}
+
+function setTodaysForecast(response) {
+    let todayForecast = document.querySelector('.today-forecast');
+    let hoursBox = document.querySelector('.hours');
+    todayForecast.style.display = "flex";
+
+    let hourlyTemp = new Array(24);
+    for (let i = 0; i < 24; i++) {
+        hourlyTemp[i] = Math.round(parseFloat(response[i].temp_f));
+    }
+    console.log(hourlyTemp);
+
+    let currentDate = Date.now();
+    let currentTime = new Date(currentDate);
+    let hours = currentTime.getHours();
+    
+    for (let i = hours; i < 24; i++) {
+        hoursBox.innerHTML += hourlyTemp[i];
+    }
+    
+    todayForecast.appendChild(hoursBox);
 }
 
 function changeSearchDisplay() {
@@ -47,21 +78,19 @@ function getCity() {
 
             console.log(response.location.name);
             console.log(response.current.temp_f + "ºF");
-            console.log(response.current.condition.icon);
 
             let cityName = document.getElementById('city');
             cityName.innerHTML = response.location.name;
 
             setCurrentTemp(response.current);
-            // console.log(response.forecast.forecastday[0].day.maxtemp_f);
-            setForecastTemp(response.forecast.forecastday[0].day);
-
+            setHighLow(response.forecast.forecastday[0].day);
+            setTodaysForecast(response.forecast.forecastday[0].hour);
             
         }
     };
 
     let days = 1;
-    let apiCall = URL+forecast+"?"+apiKey+"&"+queryParam+city+"&"+daysParam+days;
+    let apiCall = URL+FORECAST+"?"+API_KEY+"&"+QUERY_PARAM+city+"&"+DAYS_PARAM+days;
     console.log(apiCall);
     req.open("GET", apiCall);
     req.send();
