376124eaabd0b0883aee597d53691acd2c080cfa
Myles <mylesvirgilcruz@gmail.com>
2024-07-30 12:45:26 -0700

    Add current day's high and low temp

--- weather.js
+++ weather.js
@@ -1,23 +1,44 @@
 const URL = "https://api.weatherapi.com/v1";
 const apiKey = "key=c92183db87384d8d806184544242907";
 const current = "/current.json";
+const forecast = "/forecast.json";
 const queryParam = "q=";
-let apiCall = URL+current+"?"+apiKey+"&"+queryParam;
+const daysParam = "days="
 
-function setTemp(response) {
-    let details = document.querySelector('.details');
-    let cityName = document.getElementById('city');
+function setCurrentTemp(response) {
     let tempF = document.getElementById('fahrenheit');
-    let icon = document.querySelector('.icon');
-
-    cityName.innerHTML = response.location.name;
-    tempF.innerHTML = response.current.temp_f  + "ºF";
-    icon.src = response.current.condition.icon;
-    details.style.display = "flex";
+    tempF.innerHTML = Math.round(parseFloat(response.temp_f))  + "º";
 }
 
-function getCurrentTemp() {
-    let city = document.getElementById('city-search').value;
+function setForecastTemp(response) {
+    let today = document.querySelector('.today');
+    let max = Math.round(parseFloat(response.maxtemp_f));
+    let min = Math.round(parseFloat(response.mintemp_f));
+
+    today.innerHTML = "H: " + max + "º | L: " + min + "º";
+}
+
+function changeSearchDisplay() {
+    let city = document.getElementById('search');
+    city.value = "";
+    city.placeholder = '\u{1F50D}';
+    city.style.cssText = `
+        width: 20px;
+        -webkit-transition: transform 2s;
+        -moz-transition: transform 2s;
+        -o-transition: transform 2s;
+        -ms-transition: transform 2s;
+        transition: transform 2s;
+        -webkit-transform: translateX(125px);
+        -moz-transform: translateX(125px);
+        -o-transform: translateX(125px);
+        -ms-transform: translateX(125px);
+        position: absolute;
+    `;
+}
+
+function getCity() {
+    let city = document.getElementById('search').value;
 
     let req = new XMLHttpRequest();
     req.onreadystatechange = function() {
@@ -28,21 +49,29 @@ function getCurrentTemp() {
             console.log(response.current.temp_f + "ºF");
             console.log(response.current.condition.icon);
 
-            setTemp(response);
+            let cityName = document.getElementById('city');
+            cityName.innerHTML = response.location.name;
+
+            setCurrentTemp(response.current);
+            // console.log(response.forecast.forecastday[0].day.maxtemp_f);
+            setForecastTemp(response.forecast.forecastday[0].day);
+
+            
         }
     };
 
-    console.log(apiCall+city);
-    req.open("GET", apiCall+city);
+    let days = 1;
+    let apiCall = URL+forecast+"?"+apiKey+"&"+queryParam+city+"&"+daysParam+days;
+    console.log(apiCall);
+    req.open("GET", apiCall);
     req.send();
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('search').addEventListener('click', getCurrentTemp);
-
     window.addEventListener('keydown', event => {
         if (event.code === 'Enter') {
-            getCurrentTemp();
+            getCity();
+            changeSearchDisplay();
         }
     })
 });
